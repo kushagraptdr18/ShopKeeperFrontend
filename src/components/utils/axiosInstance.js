8cf4a17a46a43cdc2c162c90230803ef44781971
kushagra patidar <kushagraptdr18@gmail.com>
2025-04-08 13:25:39 +0530

    something

--- src/components/utils/axiosInstance.js
+++ src/components/utils/axiosInstance.js
@@ -3,9 +3,9 @@ import axios from 'axios';
 const axiosInstance = axios.create({
   baseURL: 'https://shopkeeperbackend.onrender.com',
   withCredentials: true,
-  // headers: {
-  //   'Content-Type': 'application/json',
-  // },
+  headers: {
+    'Content-Type': 'application/json',
+  },
 })
 
 export default axiosInstance;
